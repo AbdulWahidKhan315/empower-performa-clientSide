f8ff5dd8a7d51a2862b7bf7d0caa3b99efcddf8b
AbdulWahidKhan315 <k.wahidkhan55@gmail.com>
2023-11-24 23:12:02 +0600

    added testimonials component for homepage

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,16 @@
 import Services from "./Home/Services";
 import AutoPlay from "./Home/SliderSlik";
+import PauseOnHover from "./Home/Testimonials";
 
 
 const Home = () => {
     return (
         <div>
-            <AutoPlay></AutoPlay>    
-            <Services></Services>      
+            <AutoPlay></AutoPlay>
+            <Services></Services>
+            <div className="container mx-auto">
+                <PauseOnHover></PauseOnHover>
+            </div>
         </div>
     );
 };
